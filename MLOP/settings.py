f790b1b9164bc2d4011315db0f7a1a9a73cc9818
Joyce Zheng <joycezheng96@gmail.com>
2026-03-09 21:27:02 -0400

    update host for prod

--- MLOP/settings.py
+++ MLOP/settings.py
@@ -27,10 +27,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*'
-    ]
+ALLOWED_HOSTS = ['*']
 
-    # Allow all Azure health check internal IPs (169.254.0.0/16)
+# Allow all Azure health check internal IPs (169.254.0.0/16)
 # ALLOWED_HOSTS += [f'169.254.{i}.{j}' for i in range(256) for j in range(256)]
 
 CSRF_TRUSTED_ORIGINS = [
